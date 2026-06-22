d8a56002343d280ca650f3bfc184599251e0b8f7
Melody Chang <melodychang@Melodys-MacBook-Air.local>
2026-06-22 16:51:49 -0700

    key downs and focus features

--- app/components/lettergrid.tsx
+++ app/components/lettergrid.tsx
@@ -1,17 +1,18 @@
 interface Props {
     id: number;
-    letterInput: (letter: string) => void
+    letterInput: (letter: string, id: number, row: number) => void
+    row: number;
 }
 
 
-export function Lettergrid({ id, letterInput }: Props) {
+export function Lettergrid({ id, letterInput, row }: Props) {
     return (
-        <input name="letter" maxLength={1} type="text" onChange={(e) => letterInput(e.target.value)}  style={{
-            backgroundColor: 'lightblue', width: '50px',
+        <input name="letter" maxLength={1} type="text" onChange={(e) => letterInput(e.target.value, id, row)} style={{
+            backgroundColor: 'black', width: '50px',
             height: '50px',
             textAlign: 'center',       // Keeps typed text centered
             border: '1px solid #ccc', // Sets a clear border
             borderRadius: '0px'
-        }} className="mr-8" id={`letter${id}`} ></input>
+        }} className="mr-2 mb-8" id={`letter${id}${row}`} ></input>
     )
 };
--- app/welcome/worldle6.tsx
+++ app/welcome/worldle6.tsx
@@ -1,13 +1,45 @@
 import { Lettergrid } from "~/components/lettergrid"
 
-function LetterInput(letter: string) {
-    console.log(letter);
+
+const word: string[] = [];
+function LetterInput(letter: string, id: number, row: number) {
+    console.log(letter, id);
+    if (letter != '') {
+        const nextInput = document.querySelector(`#letter${id + 1}${row}`) as HTMLElement;
+        nextInput?.focus();
+        word.push(letter)
+    } else {
+        const prevInput = document.querySelector(`#letter${id - 1}${row}`) as HTMLElement;
+        prevInput?.focus();
+        word.pop();
+    }
+
+    const focusedInput = document.activeElement as HTMLElement;
+    focusedInput?.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowLeft") {
+            console.log("Go Back");
+            const prevInput = document.querySelector(`#letter${id}${row}`) as HTMLElement;
+            prevInput?.focus();
+        }
+    });
+
+    const lastInput = document.querySelector(`#letter${5}${row}`) as HTMLElement;
+    lastInput.addEventListener("keydown", function (event) {
+        // Check if the pressed key is "Enter"
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevent default form submission if necessary
+            console.log("Enter key was pressed!");
+            console.log(word, row)
+        }
+    });
+
 }
 
-function Letters(){
+
+function Word(row: number) {
     const letters = [];
     for (let i = 0; i < 6; i++) {
-        letters.push(<Lettergrid id={i} letterInput={LetterInput}></Lettergrid>)
+        letters.push(<Lettergrid id={i} letterInput={LetterInput} key={i} row={row}></Lettergrid>)
     }
     return letters;
 }
@@ -17,7 +49,15 @@ export function Worldle6() {
             <div>
                 <h1>Welcome to Worldle6</h1>
                 <br></br>
-                {Letters()}
+                {Word(1)}
+                <br></br>
+                {Word(2)}
+                <br></br>
+                {Word(3)}
+                <br></br>
+                {Word(4)}
+                <br></br>
+                {Word(5)}
                 <br></br>
 
             </div>
